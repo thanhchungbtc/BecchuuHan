4fa5f2d900339e9eaf8e0d3f11b8a506471751ad
Thanh Chung <thanhchungbtc@gmail.com>
2015-11-13 17:05:21 +0700

    Update MainForm

--- src/com/btc/controllers/MainForm.java
+++ src/com/btc/controllers/MainForm.java
@@ -6,17 +6,22 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JToolBar;
 import javax.swing.border.EmptyBorder;
 
 import com.btc.controllers.BecchuuKanriForm.BecchuuKanriForm;
 import com.btc.supports.Config;
 import com.btc.viewModel.BecchuuTableModel;
 import com.btc.viewModel.BukkenTableModel;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class MainForm extends JFrame {
 
@@ -24,12 +29,16 @@ public class MainForm extends JFrame {
 	private JFrame currentFrame;
 	private BecchuuKanriForm becchuuKanriForm;
 	private BecchuuIraiForm becchuuIraiForm;
+	private JPanel mainContentPanel;
 	
 	private void openNewFrame(JFrame jFrame) {
 		if (currentFrame != null) currentFrame.dispose();
 		currentFrame = jFrame;
-		this.setContentPane(jFrame.getContentPane());
-		this.setSize(jFrame.getSize());
+		mainContentPanel.removeAll();
+		
+		mainContentPanel.add(jFrame.getContentPane());
+		this.mainContentPanel.setPreferredSize(jFrame.getSize());
+		pack();
 	}
 	/**
 	 * Launch the application.
@@ -58,6 +67,11 @@ public class MainForm extends JFrame {
 		
 	}
 	
+	private void mnBecchuuKanriActionPerformed(ActionEvent event) {
+		if (becchuuKanriForm == null) becchuuKanriForm = new BecchuuKanriForm();
+			openNewFrame(becchuuKanriForm);
+	}
+	
 	private void createAndSetUpGUI() {
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -70,8 +84,7 @@ public class MainForm extends JFrame {
 		JMenuItem mnBecchuuKanri = new JMenuItem("別注管理");
 		mnBecchuuKanri.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (becchuuKanriForm == null) becchuuKanriForm = new BecchuuKanriForm();
-				openNewFrame(becchuuKanriForm);
+				mnBecchuuKanriActionPerformed(e);
 			}
 		});
 		mnFile.add(mnBecchuuKanri);
@@ -97,10 +110,36 @@ public class MainForm extends JFrame {
 		JMenuItem mntmAbout = new JMenuItem("\u30BD\u30D5\u30C8\u30A6\u30A7\u30A2\u306B\u3064\u3044\u3066");
 		mnHelp.add(mntmAbout);
 		
+		JToolBar toolbar = new JToolBar();
+		toolbar.setRollover(true);
+		toolbar.setFloatable(false);
+		toolbar.setFont(new Font("Meiryo UI", Font.PLAIN, 10));
+
+	    
+	    JButton btnBukkenKanri = new JButton("");
+	    btnBukkenKanri.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent event) {
+	    		mnBecchuuKanriActionPerformed(event);
+	    	}
+	    });
+	    btnBukkenKanri.setIcon(new ImageIcon(MainForm.class.getResource("/com/sun/java/swing/plaf/windows/icons/ListView.gif")));
+	    toolbar.add(btnBukkenKanri);
+	    
+	    JButton btnBecchuuKanri = new JButton("");
+	    btnBecchuuKanri.setIcon(new ImageIcon(MainForm.class.getResource("/com/sun/javafx/scene/web/skin/FontBackgroundColor_16x16_JFX.png")));
+	    toolbar.add(btnBecchuuKanri);
+	    toolbar.addSeparator();
+		
+	    
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		contentPane.add(toolbar, BorderLayout.NORTH);
+		mainContentPanel = new JPanel();
+		contentPane.add(mainContentPanel, BorderLayout.CENTER);
+		
 	}
 
 }
